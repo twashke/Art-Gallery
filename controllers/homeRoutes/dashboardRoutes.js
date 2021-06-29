79fe4fd18fc1bcf938961a1e5af3805d0777af63
Boba Fett <thugfencer@gmail.com>
2021-06-29 00:45:01 -0700

    Get My Dashboard working

--- controllers/homeRoutes/dashboardRoutes.js
+++ controllers/homeRoutes/dashboardRoutes.js
@@ -1,3 +1,20 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const Art = require('../../models/Art');
+
+// Display all art for main page. 
+router.get('/', async (req, res) => {
+     const artData = await Art.findAll().catch((err) => {
+          res.json(err);
+     });
+     const arts = artData.map((art) => art.get({ plain: true }));
+     res.render('collection', { arts });
+});
+
+//Display art details
+router.get('/:id', async (req, res) => {
+     const detData = await Art.findByPk(req.params.id);
+     const det = detData.get({ plain: true });
+     res.render('collectionDetail', { det });
+});
 
 module.exports = router;
