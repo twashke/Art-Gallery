8da6bbb6b3c93dd7a5c3427b4e71bea66676c4e5
Tiffany Washke <twashke@gmail.com>
2021-07-01 21:48:28 -0700

    updated seeds for login and art was distributed among the users

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,24 +2,28 @@ const { User } = require("../models/index");
 
 const userData = [
      {
-          username: "Sal",
-          email: "sal@hotmail.com",
-          password: "password12345",
+          username: "jack",
+          email: "jack@daniels.com",
+          // password12345 use for login
+          password: "$2b$10$HPcu.H61TPEWlWO51kWySuINwhtAhkkh65tFrXesb31gCbj/zBCEu",
      },
      {
-          username: "Lernantino",
-          email: "lernantino@gmail.com",
-          password: "password12345",
+          username: "jose",
+          email: "jose@cuervo.com",
+          // password12345 use for login
+          password: "$2b$10$C2Y9drc3RV208DIoEMNW4.J7Wa9F6v0QsBI7oiLqMvh2vnmKLCQ5i",
      },
      {
-          username: "Amiko",
-          email: "amiko2k20@aol.com",
-          password: "password12345",
+          username: "jim",
+          email: "jim@bean.com",
+          // password12345 use for login
+          password: "$2b$10$.vknOXafaRndzpwc4MMjzusGWNUzg5P14KvKNgiMLL96gutlaQ7Dy",
      },
      {
-          username: "Thugfencer",
-          email: "thugfencer@thuglife.com",
-          password: "12345abcde",
+          username: "jill",
+          email: "jill@valentine.com",
+          // password12345 use for login
+          password: "$2b$10$bqd5z4XwqUl9iUreG8QrIO4jZHYelfZ05cIbQeQKkJ2cEIQunKmkK",
      },
 
 ];
