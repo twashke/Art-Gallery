aff6939d5ced40ef4966d7c7239b39146f8014cd
Tiffany Washke <twashke@gmail.com>
2021-07-04 16:19:33 -0700

    update for Heroku

--- config/cloudinaryconfig.js
+++ config/cloudinaryconfig.js
@@ -3,12 +3,16 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+if (process.env.CLOUDINARY_URL) {
+  cloudinary.config(process.env.CLOUDINARY_URL)
+} else {
 //configuration for cloundinary
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
+};
 
 //storage
 const storage = new CloudinaryStorage({
