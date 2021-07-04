dea3f0e0fb8871277fb5f428ae767c73bca0fa95
Tiffany Washke <twashke@gmail.com>
2021-07-04 16:28:45 -0700

    reverted changes

--- config/cloudinaryconfig.js
+++ config/cloudinaryconfig.js
@@ -3,16 +3,12 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-if (process.env.CLOUDINARY_URL) {
-  cloudinary.config(process.env.CLOUDINARY_URL)
-} else {
 //configuration for cloundinary
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
-};
 
 //storage
 const storage = new CloudinaryStorage({
@@ -22,8 +18,6 @@ const storage = new CloudinaryStorage({
     allowedFormats: ["jpg", "png"],
   },
 });
-
-
 
 const artUpload = multer({ storage: storage });
 
