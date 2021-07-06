a90bb5b6fa37e45db590ba8cb544f600def5e233
Boba Fett <thugfencer@gmail.com>
2021-07-05 22:02:26 -0700

    add helper function for thumbnails to increase performance

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,21 @@
-// Helper to format date
+// Helper to build thumbnail URL which speeds up the displaying of cards for collection and dashboard
 module.exports = {
-  format_time: (date) => {
-    return date.toLocaleTimeString();
-  },
-  format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear()
-    }`;
-  },
+
+     format_time: (date) => {
+          return date.toLocaleTimeString();
+     },
+
+     format_date: (date) => {
+          return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()
+               }`;
+     },
+
+     thumbnail: (url) => {
+          var urla = url.substr(0, 50);
+          var urlb = "c_thumb,w_300,g_face/";
+          var urlc = url.substr(50, url.length);
+          var newurl = urla.concat(urlb).concat(urlc);
+          //console.log(newurl);
+          return newurl;
+     },
 };
