41d63c1e1244c7c9a51e7ad78daf79c32ec5dc62
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-07-07 14:59:45 -0700

    removed console

--- controllers/homeRoutes/dashboardRoutes.js
+++ controllers/homeRoutes/dashboardRoutes.js
@@ -3,41 +3,47 @@ const { Art, User, Comment } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
-     try {
-          const userArtDetails = await User.findOne({
-               where: {
-                    id: req.session.user_id,
-               },
-               attributes: ["id"],
-               include: [
-                    {
-                         model: Art,
-                         attributes: ["id", "img_url", "art_name", "technique", "dimension", "price", "other_details"],
-                    },
-               ],
-          });
-          console.log(userArtDetails);
-          const userArts = userArtDetails.get({ plain: true });
-          console.log(userArts);
-          res.render("dashboard", {
-               userArts,
-               loggedIn: true,
-          });
-     } catch (err) {
-          console.log(err);
-          res.status(500).json(err);
-     }
+  try {
+    const userArtDetails = await User.findOne({
+      where: {
+        id: req.session.user_id,
+      },
+      attributes: ["id"],
+      include: [
+        {
+          model: Art,
+          attributes: [
+            "id",
+            "img_url",
+            "art_name",
+            "technique",
+            "dimension",
+            "price",
+            "other_details",
+          ],
+        },
+      ],
+    });
+    const userArts = userArtDetails.get({ plain: true });
+    res.render("dashboard", {
+      userArts,
+      loggedIn: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/add", (req, res) => {
-     res.render("dashboardAddArt", {
-          loggedIn: true,
-     });
+  res.render("dashboardAddArt", {
+    loggedIn: true,
+  });
 });
 
 router.get("/update/:id", async (req, res) => {
-     const artDetails = await Art.findByPk(req.params.id);
-     const art = artDetails.get({ plain: true });
-     res.render("dashboardDetail", { art, loggedIn: true });
+  const artDetails = await Art.findByPk(req.params.id);
+  const art = artDetails.get({ plain: true });
+  res.render("dashboardDetail", { art, loggedIn: true });
 });
 module.exports = router;
