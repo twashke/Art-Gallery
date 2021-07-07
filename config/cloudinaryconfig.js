4f57a1c71782cc573a589234e4b619614bc0f466
Tiffany Washke <twashke@gmail.com>
2021-07-06 18:34:04 -0700

    updated to remove comments

--- config/cloudinaryconfig.js
+++ config/cloudinaryconfig.js
@@ -3,15 +3,12 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-//configuration for cloundinary
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
 
-
-//storage
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
