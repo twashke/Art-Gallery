5d2b60713e8852c14458e6853e40079f9053110e
Tiffany Washke <twashke@gmail.com>
2021-07-06 19:42:37 -0700

    updated js files to remove comments and console logs

--- public/js/deleteart.js
+++ public/js/deleteart.js
@@ -1,7 +1,6 @@
 async function deleteAnArt(event) {
   if (event.target.hasAttribute("data-id")) {
     const id = event.target.getAttribute("data-id");
-    // console.log("@@@@@@@@@@@@@@@@@@@@@@@", id);
     const response = await fetch(`/api/arts/${id}`, {
       method: "DELETE",
     });
@@ -14,5 +13,4 @@ async function deleteAnArt(event) {
   }
 }
 
-// document.querySelector("#deleteart").addEventListener("click", deleteAnArt);
 document.querySelector("#allartsbyuser").addEventListener("click", deleteAnArt);
